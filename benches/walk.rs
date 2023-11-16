d9c4a28151f1002901bdc688108372f85d685657
zX3no <28522740+zX3no@users.noreply.github.com>
2023-11-16 17:06:23 +1030

    Improved benchmarks

--- benches/walk.rs
+++ benches/walk.rs
@@ -7,16 +7,17 @@ const DEPTH: usize = 1;
 
 #[divan::bench]
 fn winwalk() {
-    let results = winwalk::walkdir(divan::black_box(PATH), DEPTH);
+    use winwalk::*;
+    let results: Vec<Result<DirEntry, Error>> = walkdir(divan::black_box(PATH), DEPTH);
     assert!(!results.is_empty());
 }
 
 #[divan::bench]
 fn walkdir() {
-    let results: Vec<_> = walkdir::WalkDir::new(divan::black_box(PATH))
+    use walkdir::{DirEntry, Error, WalkDir};
+    let results: Vec<Result<DirEntry, Error>> = WalkDir::new(divan::black_box(PATH))
         .max_depth(DEPTH)
         .into_iter()
-        .flatten()
         .collect();
     assert!(!results.is_empty());
 }

5e2ef4cf99b176afcb014d250e2b5c2361114cdd
zX3no <28522740+zX3no@users.noreply.github.com>
2023-11-16 17:01:17 +1030

    Cleanup benchmarks

--- benches/walk.rs
+++ benches/walk.rs
@@ -1,18 +1,18 @@
-const PATH: &str = "C:\\Windows\\System32";
-const DEPTH: usize = 1;
-
 fn main() {
     divan::main();
 }
 
+const PATH: &str = "C:\\Windows\\System32";
+const DEPTH: usize = 1;
+
 #[divan::bench]
-fn local() {
+fn winwalk() {
     let results = winwalk::walkdir(divan::black_box(PATH), DEPTH);
     assert!(!results.is_empty());
 }
 
 #[divan::bench]
-fn walk() {
+fn walkdir() {
     let results: Vec<_> = walkdir::WalkDir::new(divan::black_box(PATH))
         .max_depth(DEPTH)
         .into_iter()
